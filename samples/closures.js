17a77e2c96c9c37d4cbec4f8f6485ad6ed61957d
David Lee <david-lee@users.noreply.github.com>
2016-05-17 23:46:59 -0400

    Update closures.js

--- samples/closures.js
+++ samples/closures.js
@@ -2,7 +2,7 @@
 var buttons = document.querySelectorAll('.btn');
 var len = buttons.length;
 
-for (i = 0; i < len; i++) {
+for (var i = 0; i < len; i++) {
   buttons[i].addEventListener('click', function() {
     alert('button-'+ i + ' clicked.');
   });
